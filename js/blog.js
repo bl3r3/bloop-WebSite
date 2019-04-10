3c3c619477d093b5493f852532beefef8d710e94
Roberto Carrasquel <carrasquel18@gmail.com>
2019-04-09 22:19:24 -0400

    Carosuel

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+  $(".blog").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 4,
+    navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  });
--- js/index.js
+++ js/index.js
@@ -1,53 +1,9 @@
-$(document).ready(function() {
-    $("#myCarousel").on("slide.bs.carousel", function(e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $("#myCarousel .carousel-item").length;
-  
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-          // añade cards al final
-          if (e.direction == "left") {
-            $("#myCarousel .carousel-item")
-              .eq(i)
-              .appendTo("#myCarousel .carousel-inner");
-          } else {
-            $("#myCarousel .carousel-item")
-              .eq(0)
-              .appendTo($(this).find(".carousel-inner"));
-          }
-        }
-      }
-    });
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 6,
+    navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
   });
-  
-  
-  $(document).ready(function() {
-    $("#myCarousel1").on("slide.bs.carousel", function(e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $("#myCarousel1 .carousel-item").length;
-  
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-  
-        for (var i = 0; i < it; i++) {   
-          // append slides to end
-          if (e.direction == "left") {
-            $("#myCarousel1 .carousel-item")
-              .eq(i)
-              .appendTo("#myCarousel1 .carousel-inner");
-          } else {
-            $(".carousel-item")
-              .eq(0)
-              .appendTo($(this).find(".carousel-inner"));
-          }
-          
-        }
-      }
-    });
-  });
-  
+
+});
