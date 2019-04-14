d1b27b877c27a19ee838e5751ee9b538a029b324
Roberto Carrasquel <carrasquel18@gmail.com>
2019-04-13 21:24:41 -0400

    responsive desing 80

--- js/index.js
+++ js/index.js
@@ -26,4 +26,29 @@ $(document).ready(function(){
     }
   });
 
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+      if (scroll > 1) {
+        $(".navbar").css("background" , "#29a9e1");
+      }
+
+      else{
+          $(".navbar").css("background" , "transparent");   
+      }
+  })
+ 
+ 
+ $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+      if (scroll === 0) {
+        $(".navnav").removeClass("active-menu").addClass("default-menu");
+      }else {
+          $(".navnav").addClass("active-menu").removeClass("default-menu")
+      }
+  })
+
+
+ 
+
 });
+
